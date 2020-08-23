39c88efdfc0d209fd3baf3b51fee06a354b19cd2
ali.alkhalidi <ali.alkhalidi@securekey.com>
2020-08-22 22:37:00 -0400

    temporary and dumb way for retry/back-off; will fix later
    Signed-off-by: ali.alkhalidi <ali.alkhalidi@securekey.com>

--- src/gmlserver/recover_lock_box.go
+++ src/gmlserver/recover_lock_box.go
@@ -5,6 +5,11 @@ import (
 	"time"
 )
 
+const (
+	retry_attempts = 3
+	retry_backoff  = 10
+)
+
 func RecoverLockboxWithClientID(accessToken string, expectedStatus int, clientID string) (string, *RecoverLockboxRespBody, error) {
 	payload := &RecoverLockboxReqBody{
 		AccessToken: accessToken,
@@ -18,8 +23,18 @@ func RecoverLockboxWithClientID(accessToken string, expectedStatus int, clientID
 	// give a few seconds for pre-conditions to propagte
 	time.Sleep(10 * time.Second)
 	var expected = new(RecoverLockboxResp)
+	retries := 0
+retry:
 	err := SendRequestAndCheckResponse(strings.ToLower(RequestMethodRecoverLockbox), req.Body, expectedStatus, &expected.Body)
 	if err != nil {
+		if strings.Contains(err.Error(), "504") {
+			if retries < 3 {
+				retries++
+				myLogger.Printf("RecoverLockboxWithClientID: recieved gateway 504 timeout, retry attempt %d/%d with %ds backoff", retries, retry_attempts, retry_backoff)
+				time.Sleep(retry_backoff * time.Second)
+				goto retry
+			}
+		}
 		return "", nil, err
 	}
 	return expected.Body.ServerState, expected.Body.RecoverLockboxBody, nil
